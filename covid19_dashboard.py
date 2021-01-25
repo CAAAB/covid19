2df7a4247e612974dd8bb7528de45258853ac650
CAAAB <charles.borel@alumni.epfl.ch>
2021-01-25 23:28:25 +0100

    Update covid19_dashboard.py

--- covid19_dashboard.py
+++ covid19_dashboard.py
@@ -3,6 +3,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 from datetime import datetime, timedelta
 import streamlit as st
+import streamlit.components.v1 as components
 
 PAGE_CONFIG = {"page_title":"Covid-19 dashboard","page_icon":":mask:","layout":"wide"}
 st.set_page_config(**PAGE_CONFIG)
@@ -13,6 +14,17 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+components.html(
+"""
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WW9GXX9');</script>
+<!-- End Google Tag Manager -->
+"""
+)
 def main():
     premade = True
     def make_df2():
